8bc78114fb2217c52e3bbd4d6a3b2ded3b591b7d
Cedrat <loicnoai@gmail.com>
2021-02-02 20:08:20 +0100

    Fix wordpress bug and ftps

--- setup.sh
+++ setup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 sudo apt-get install conntrack
 minikube start --vm-driver=none
-sed -i  "s/x.x.x.x/$(sudo minikube ip)/g" config.yaml
-sed -i  "s/x.x.x.x/$(sudo minikube ip)/g" mysql/srcs/wordpress.sql
+sed -i  "s/x.x.x.x/$(sudo minikube ip)/g" srcs/config.yaml
+sed -i  "s/x.x.x.x/$(sudo minikube ip)/g" srcs/mysql/srcs/wordpress.sql
 eval $(minikube docker-env)
-docker build ./nginx -t nginx
-docker build ./wordpress -t wordpress
-docker build ./phpmyadmin -t phpmyadmin
-docker build ./mysql -t mysql
-docker build ./grafana -t grafana
-docker build ./influxdb -t influxdb
-docker build ./ftps -t ftps
+docker build ./srcs/nginx -t nginx
+docker build ./srcs/wordpress -t wordpress
+docker build ./srcs/phpmyadmin -t phpmyadmin
+docker build ./srcs/mysql -t mysql
+docker build ./srcs/grafana -t grafana
+docker build ./srcs/influxdb -t influxdb
+docker build ./srcs/ftps -t ftps
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
-kubectl apply -f config.yaml
-kubectl apply -f deployment_nginx.yaml
-kubectl apply -f deployment_phpmyadmin.yaml
-kubectl apply -f deployment_mysql.yaml
-kubectl apply -f deployment_wordpress.yaml
-kubectl apply -f deployment_grafana.yaml
-kubectl apply -f deployment_influxdb.yaml
-kubectl apply -f deployment_ftps.yaml
-sed -i  "s/$(sudo minikube ip)/x.x.x.x/g" mysql/srcs/wordpress.sql
-sed -i  "s/$(sudo minikube ip)/x.x.x.x/g" config.yaml
+kubectl apply -f srcs/config.yaml
+kubectl apply -f srcs/deployment_nginx.yaml
+kubectl apply -f srcs/deployment_phpmyadmin.yaml
+kubectl apply -f srcs/deployment_mysql.yaml
+kubectl apply -f srcs/deployment_wordpress.yaml
+kubectl apply -f srcs/deployment_grafana.yaml
+kubectl apply -f srcs/deployment_influxdb.yaml
+kubectl apply -f srcs/deployment_ftps.yaml
+sed -i  "s/$(sudo minikube ip)/x.x.x.x/g" srcs/mysql/srcs/wordpress.sql
+sed -i  "s/$(sudo minikube ip)/x.x.x.x/g" srcs/config.yaml
--- srcs/ftps/srcs/start.sh
+++ srcs/ftps/srcs/start.sh
@@ -2,6 +2,7 @@
 openrc
 touch /run/openrc/softlevel
 rc-service telegraf start
+chown root /etc/vsftpd/vsftpd.conf
 rc-service vsftpd start
 sleep 10
 tail -f /dev/null
